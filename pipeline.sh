718f10fe1a75cc340169c1393bd70600248f9788
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2013-11-22 11:08:24 +0100

    Added cli flags, get input, check permissions, directory creation

--- pipeline.sh
+++ pipeline.sh
@@ -1,50 +1,189 @@
-#!/bin/bash  
+#!/bin/bash
+
+# USAGE
+# <scriptname> input_fastq_file REFERENCE_GENOME ANNOTATION_FILE
+function print_usage { echo "Usage: -s <SEQUENCE_FILE> -r <GENOME_REFERENCE FILE (fasta)> -g <GENOME_FEATURE_FILE (gtf/gff)> -d <WORKING_DIRECTORY>." >&2 ; }
+
+
 #
 #Create a project directory containing a script directory with all scripts and a reference directory with bowtie refs and gff file.
 # as well as a seqdata directory with the raw sequencing data.
 #For the moment bowtie has to be under bowtie-1.0.0/ in the project directory.
 #
-# Take Input and contruct directory structure        
-BASE=$PWD
-DATE=$(date "+%Y%m%d")
-INFILE=$1
-SUFFIX=$2
-REFBASE=$3
-BARCODED=$4
-NEWSUFFIX=$2
-LOGFILE='../logs/'$(basename "$INFILE" $SUFFIX)"_"$DATE".log"
-#
-#  project/seqdata, aligned, annotated, scripts, logs
-#
-mkdir ../logs ../seqdata ../aligned ../annotated ../references #, ../plots
 
-cd ../seqdata
-# Move barcode to header
-if [ $BARCODED == 'barcoded' ]
-then
-	NEWSUFFIX='_barcoded.fastq'
-	OUTFILE_BARCODING=$(basename "$INFILE" $SUFFIX)$NEWSUFFIX
-	if [ ! -f $OUTFILE_BARCODING ]
-	then
-    	python ../scripts/oop.py 'fq_barcoding' $INFILE $OUTFILE_BARCODING
-	else
-    	echo 'Barcoding already performed'
-    fi
-    INFILE=$OUTFILE_BARCODING
+
+# GET INPUT
+while getopts ":s:r:g:d:" opt; do
+    case $opt in
+        s)
+            SEQ_FILE=$OPTARG
+            ;;
+        r)
+            GENOME_REF=$OPTARG
+            ;;
+        g)
+            GFF_FILE=$OPTARG
+            ;;
+        d)
+            WORK_DIR=$OPTARG
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            print_usage
+            exit 1;
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            print_usage
+            exit 1;
+            ;;
+    esac
+done
+
+
+# CHECK INPUTS
+
+if [[ ! $SEQ_FILE ]]; then
+    echo "Must specify input sequence file (-s)."
+    print_usage
+    exit 1;
 fi
 
-# Remove adaptor and throw away too short reads
-SUFFIX=$NEWSUFFIX
-NEWSUFFIX='_trimmed.fastq'
-INFILE_CUTADAPT=$INFILE
-OUTFILE_CUTADAPT=$(basename "$INFILE_CUTADAPT" $SUFFIX)$NEWSUFFIX
-ADAPTOR="TGGAATTCTCGGGTGCCAAGG"
-#Think it's the TruSeq adapter   "TGGAATTCTCGGGTGCCAAGG"
+if [[ $GENOME_REF ]] || [[ $GFF_FILE ]]; then
+    [[ $GENOME_REF ]] || echo "Warning: no genome reference file (-r) specified; will not perform alignment."
+    [[ $GFF_FILE ]] || echo "Warning: no genome feature file (-g) specified; will not perform annotation or feature counting."
+else
+    echo "No genome reference file (-r) or genome feature file (-g) specified; nothing to do. Exiting."
+    print_usage
+    exit
+fi
+
+for file in $SEQ_FILE $GENOME_REF $GFF_FILE; do
+    if [[ ! -e $file ]]; then
+        echo "File does not exist: $file"
+        exit
+    elif [[ ! -r $file ]]; then
+        echo "File cannot be read: $file"
+        exit
+    fi
+done
+
+if [[ ! $WORK_DIR ]]; then
+    echo "Info:    no working directory (-d) specified; using '$PWD'"
+    WORK_DIR=$PWD
+fi
+echo "WORK_DIR: $WORK_DIR"
+
+# INPUT / DIRECTORY TREE CONSTRUCTION
+
+DATE=$(date "+%Y%m%d")
+
+INPUTFILE=$(basename $SEQ_FILE)
+INPUTFILE_ABSPATH=$(readlink -f $SEQ_FILE)
+INPUTFILE_DIR=$(dirname $INPUTFILE_ABSPATH)
+INPUTFILE_BASE="${INPUTFILE%.*}"
+INPUTFILE_EXTENSION="${INPUTFILE##*.}"
+
+#echo "INPUTFILE:            $INPUTFILE"
+#echo "INPUTFILE_ABSPATH:    $INPUTFILE_ABSPATH"
+#echo "INPUTFILE_DIR:        $INPUTFILE_DIR"
+#echo "INPUTFILE_BASE:       $INPUTFILE_BASE"
+#echo "INPUTFILE_EXTENSION:  $INPUTFILE_EXTENSION"
+
+if [[ $GENOME_REF ]]; then
+    REFERENCE=$(basename $GENOME_REF)
+    REFERENCE_ABSPATH=$(readlink -f $GENOME_REF)
+    REFERENCE_DIR=$(dirname $REFERENCE_ABSPATH)
+    REFERENCE_BASE="${REFERENCE%.*}"
+    REFERENCE_EXTENSION="${REFERENCE##*.}"
+fi
+
+#echo "REFERENCE:            $REFERENCE"
+#echo "REFERENCE_ABSPATH:    $REFERENCE_ABSPATH"
+#echo "REFERENCE_DIR:        $REFERENCE_DIR"
+#echo "REFERENCE_BASE:       $REFERENCE_BASE"
+#echo "REFERENCE_EXTENSION:  $REFERENCE_EXTENSION"
+
+
+if [[ $GFF_FILE ]]; then
+    GFF_FILE=$(basename $GFF_FILE)
+    GFF_FILE_ABSPATH=$(readlink -f $GFF_REF)
+    GFF_FILE_DIR=$(dirname $GFF_FILE_ABSPATH)
+    GFF_FILE_BASE="${GFF_FILE%.*}"
+    GFF_FILE_EXTENSION="${GFF_FILE##*.}"
+fi
+
+#echo "GFF_FILE:            $GFF_FILE"
+#echo "GFF_FILE_ABSPATH:    $GFF_FILE_ABSPATH"
+#echo "GFF_FILE_DIR:        $GFF_FILE_DIR"
+#echo "GFF_FILE_BASE:       $GFF_FILE_BASE"
+#echo "GFF_FILE_EXTENSION:  $GFF_FILE_EXTENSION"
+
+# Directories
+LOG_DIR=$WORK_DIR"/logs/"
+SEQDATA_DIR=$WORK_DIR"/seqdata/"
+ALIGNED_DIR=$WORK_DIR"/aligned/"
+ANNOTATED_DIR=$WORK_DIR"/annotated/"
+LOGFILE=$LOG_DIR/$INPUFILET_BASE$DATE".log"
+for dir in $LOG_DIR $SEQDATA_DIR $ALIGNED_DIR $ANNOTATED_DIR; do
+    if [[ -e $dir ]]; then
+        # TODO add -f flag to force overwrite
+        echo "Directory '$dir' exists. Exiting."
+        exit
+    elif [[ $(mkdir -p $dir) -ne 0 ]]; then
+        echo "Cannot create directory $dir; exiting."
+        exit
+    fi
+done
+
+#echo "LOG_DIR:          $LOG_DIR"
+#echo "SEQDATA_DIR:      $SEQDATA_DIR"
+#echo "ALIGNED_DIR:      $ALIGNED_DIR"
+#echo "ANNOTATED_DIR:    $ANNOTATED_DIR"
+#echo "LOGFILE:          $LOGFILE"
+
+exit
+
+# MODULE LOADING
+# Modules, activate the module command
+case "$0" in
+          -sh|sh|*/sh)  modules_shell=sh ;;
+       -ksh|ksh|*/ksh)  modules_shell=ksh ;;
+       -zsh|zsh|*/zsh)  modules_shell=zsh ;;
+    -bash|bash|*/bash)  modules_shell=bash ;;
+esac
+module() { eval `/usr/local/Modules/$MODULE_VERSION/bin/modulecmd $modules_shell $*`; } 
+module load cutadapt
+module load bowtie2
+
+# I'm not sure what this code is for -- I'm guessing if your samples aren't demultiplexed?
+# Move barcode to header
+#if [ $BARCODED == 'barcoded' ]
+#then
+#	NEWSUFFIX='_barcoded.fastq'
+#	OUTFILE_BARCODING=$(basename "$INFILE" $SUFFIX)$NEWSUFFIX
+#	if [ ! -f $OUTFILE_BARCODING ]
+#	then
+#    	python ../scripts/oop.py 'fq_barcoding' $INFILE $OUTFILE_BARCODING
+#	else
+#    	echo 'Barcoding already performed'
+#    fi
+#    INFILE=$OUTFILE_BARCODING
+#fi
+#
+
+# ADAPTER TRIMMING
+# Remove 3' adapter sequences, discarding reads shorter than $LENGTH
+PRESUFFIX='_trimmed'
+INFILE_CUTADAPT=$INPUTFILE_ABSPATH
+OUTFILE_CUTADAPT=$INPUTFILE_DIR$INPUTFILE_BASE$PRESUFFIX$INPUTFILE_EXTENSION
+# TruSeq adapter sequence "TGGAATTCTCGGGTGCCAAGG"
+ADAPTER="TGGAATTCTCGGGTGCCAAGG"
 LENGTH='18'
 
 if [ ! -f $OUTFILE_CUTADAPT ]
 then
-    cutadapt -a $ADAPTOR --match-read-wildcards -O 5 -m $LENGTH --too-short-output=/dev/null -o $OUTFILE_CUTADAPT $INFILE_CUTADAPT | tee $LOGFILE
+    cutadapt -a $ADAPTER --match-read-wildcards -O 5 -m $LENGTH --too-short-output=/dev/null -o $OUTFILE_CUTADAPT $INFILE_CUTADAPT | tee $LOGFILE
 else
 	echo 'Trimming already performed.'
 fi
