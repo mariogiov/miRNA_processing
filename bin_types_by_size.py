8bc48e809b8551fa5d2c42ea5a0860c6a24fd2b8
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2013-12-09 22:21:18 +0100

    Tmp save counts to yaml file

--- bin_types_by_size.py
+++ bin_types_by_size.py
@@ -7,32 +7,42 @@ import argparse
 import collections
 import counts
 import HTSeq
+import numpy as np
 import os
 import shlex
 import sys
+import yaml
 
-from cStringIO import StringIO
+#from cStringIO import StringIO
 from subprocess import Popen, PIPE
 
-def main(aln_file, ann_file, ref_file, output_dir, id_attr='ID', feature_type=None):
+def main(aln_file, ann_file, ref_file, output_dir=None, id_attr='ID', feature_type=None):
 
+    if not output_dir:
+        output_dir = os.path.join(os.getcwd(), "annotated")
+        print("No output directory specified; using {output_dir}".format(**locals()), file=sys.stderr)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Get feature types from GFF file
     if not feature_type:
         feature_types = sorted(get_feature_types(ann_file).keys())
     else:
         feature_types = [feature_type]
 
-    for feature_type in feature_types:
-        print("Getting counts for feature type \"{feature_type}\" in alignment file \"{aln_file}\" "
-                "from annotation file \"{ann_file}\"".format(**locals()), file=sys.stderr)
+    feature_counts = {}
 
-        # counts_reads_in_features prints to stdout
-        sys.stdout = mystdout = StringIO()
-        try:
-            # Usage: count_reads_in_features( sam_filename, gff_filename, stranded, overlap_mode, feature_type, id_attribute, quiet, minaqual, samout )
-            counts.count_reads_in_features(aln_file, ann_file, False, "union", feature_type, 'ID', False, 10, "")
-        except ValueError:
-            pass
-        sys.stdout = sys.__stdout__
+    # Annotate
+    for feature_type in feature_types:
+        # TODO here it probably makes more sense to pull the gff file into memory to keep from reloading it each time
+        # or to use the get_features_given_type to simulate a file only containing those features
+        length_counts = annotate_count_aln(aln_file, ann_file, output_dir, feature_type)
+        feature_counts[feature_type] = length_counts
+
+    in_base, _ = os.path.splitext(os.path.basename(aln_file))
+    tmp_output_file = os.path.join(output_dir, "feature_counts-{}.tsv".format(in_base))
+    with open(tmp_output_file, 'w') as f:
+        f.write( yaml.dump(feature_counts) )
 
 
 def get_feature_types(annotation_file):
@@ -59,8 +69,40 @@ def get_features_given_type(gff_file, feature_type):
 
     return features_dict
 
+def annotate_count_aln(aln_file, ann_file, output_dir, feature_type):
+    """
+    Annotated some alignments, and count that shit
+    """
 
-def count_features_in_aln(aln_file, ann_file, feature_type, output_dir=None, reference_file=None):
+    aln_file_base, _    = os.path.splitext(os.path.basename(aln_file))
+    ann_file_base, _    = os.path.splitext(os.path.basename(ann_file))
+    ann_sam_file        = os.path.join(output_dir, "{aln_file_base}_annotated_{ann_file_base}-{feature_type}.sam".format(**locals()))
+    ann_cnt_file        = os.path.join(output_dir, "{aln_file_base}_annotated_{ann_file_base}-{feature_type}.counts.tsv".format(**locals()))
+
+    print("Getting counts for feature type \"{feature_type}\" in alignment file \"{aln_file}\" "
+          "from annotation file \"{ann_file}\"".format( feature_type=feature_type, aln_file=os.path.basename(aln_file),
+                                                        ann_file=os.path.basename(ann_file)), file=sys.stderr)
+
+    # counts_reads_in_features prints to stdout
+    old_stdout = sys.stdout
+    #sys.stdout = counts_fh = open(ann_cnt_file, 'w')
+    sys.stdout = f = open(os.devnull, 'w')
+
+    length_counts = None
+    try:
+        # Usage: count_reads_in_features( sam_filename, gff_filename, stranded, overlap_mode, feature_type, id_attribute, quiet, minaqual, samout )
+        #return counts.count_reads_in_features(aln_file, ann_file, False, "union", feature_type, 'ID', False, 10, ann_sam_file)
+        return counts.count_reads_in_features(aln_file, ann_file, False, "union", feature_type, 'ID', False, 10, "")
+    except ValueError:
+        return None
+    finally:
+        sys.stdout = old_stdout
+        f.close()
+
+    #return length_counts, ann_sam_file, ann_cnt_file
+
+
+def count_features_in_aln_manual(aln_file, ann_file, feature_type, output_dir=None, reference_file=None):
     """
     Given an alignment file, an annotation file, and an RNA type,
     returns the number of counts of that feature type present in the alignment.
